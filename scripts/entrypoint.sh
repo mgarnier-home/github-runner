4a5c75f7b0c6cd97dff15c6781b5e5f8ed4313c8
mgarnier11 <mgarnier11@gmail.com>
2026-01-20 17:14:54 +0100

    fix: test echo to check latency

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -321,11 +321,16 @@ if [[ ${_RUN_AS_ROOT} == "true" ]]; then
     exit 1
   fi
 else
+  echo "$(date)"
   if [[ $(id -u) -eq 0 ]]; then
+    echo "$(date)"
     [[ -n "${_CONFIGURED_ACTIONS_RUNNER_FILES_DIR}" ]] && chown -R runner "${_CONFIGURED_ACTIONS_RUNNER_FILES_DIR}"
+    echo "$(date)"
     chown -R runner "${_RUNNER_WORKDIR}" /actions-runner
+    echo "$(date)"
     # The toolcache is not recursively chowned to avoid recursing over prepulated tooling in derived docker images
     chown runner /opt/hostedtoolcache/
+    echo "$(date)"
     if [[ ${_DEBUG_ONLY} == "true" ]] || [[ ${_DEBUG_OUTPUT} == "true" ]] ; then
       # shellcheck disable=SC2145
       echo "Running /usr/sbin/gosu runner $@"
